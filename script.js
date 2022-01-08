02b8623815af1f58c7646d9539e2abdd7ebbd025
NEWUSER <l00169829@student.lyit.ie>
2022-01-08 19:37:33 +0000

    new buttons and width constraints

--- script.js
+++ script.js
@@ -24,8 +24,28 @@ function showSeanfhocal(x){
         seanfhocalText.innerText = "Is iad na muca ciúine a itheann an mhin."
     }
     else if (x == 4){
-        console.log("clicked3")
+        console.log("clicked4")
         seanfhocalText.innerText = "Is fada an bóthar nach bhfuil casadh ann."
+    }
+    else if (x == 5){
+        console.log("clicked5")
+        seanfhocalText.innerText = "An rud a scríobhann an púca, léann sé féin é."
+    }
+    else if (x == 6){
+        console.log("clicked6")
+        seanfhocalText.innerText = "Aithníonn ciaróg ciaróg eile."
+    }
+    else if (x == 7){
+        console.log("clicked7")
+        seanfhocalText.innerText = "Is fearr rith maith ná drochsheasamh."
+    }
+    else if (x == 8){
+        console.log("clicked8")
+        seanfhocalText.innerText = "Is glas iad na cnoic i bhfad uainn."
+    }
+    else if (x == 9){
+        console.log("clicked8")
+        seanfhocalText.innerText = "Is minic a bhí cú mhall sona."
     }
     
 
